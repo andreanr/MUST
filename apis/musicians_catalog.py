12226e6e224086b239f1413a0da9eda79e964b4e
Omar Pardo <omarpardog@gmail.com>
2019-03-08 12:26:13 -0500

    Update apis functions, to write into tables instead of csv.

--- apis/musicians_catalog.py
+++ apis/musicians_catalog.py
@@ -5,21 +5,25 @@ import pandas as pd
 import os
 from dotenv import load_dotenv, find_dotenv
 
-
 # Load environment variables
 load_dotenv(find_dotenv())
-
 
 # Assign KEYS
 SPOTIFY_CLIENT_ID = os.environ.get('SPOTIFY_CLIENT_ID')
 SPOTIFY_CLIENT_SECRET = os.environ.get('SPOTIFY_CLIENT_SECRET')
 
-def create_empty_tables():
-    """Creates empty csv files with specified columns"""
-    
-    # Musicians catalog
-    musicians = pd.DataFrame(columns=['sp_id', 'name'])
-    musicians.to_csv('must_data/musicians.csv', index=False)
+# PostgreSQL credentials
+PGDATABASE = os.environ.get('PGDATABASE')
+PGPASSWORD = os.environ.get('PGPASSWORD')
+PGUSER = os.environ.get('PGUSER')
+PGHOST = os.environ.get('PGHOST')
+
+engine = sqlalchemy.create_engine('postgresql://{user}:{password}@{host}/{database}'.format(
+                                  host = PGHOST,
+                                  database = PGDATABASE,
+                                  user = PGUSER,
+                                  password = PGPASSWORD))
+
 
 def create_authorized_spotify_object():
     """
@@ -41,7 +45,10 @@ def create_popular_musician_record(musician_name):
         popular_musician['name'] = musician['name']
 
         popular_musician = pd.DataFrame([popular_musician], columns=['sp_id', 'name'])
-        popular_musician.to_csv('must_data/musicians.csv', mode='a', index=False, header=False)
+        #popular_musician.to_csv('must_data/musicians.csv', mode='a', index=False, header=False)
+        db_conn = engine.connect()
+        popular_musician.to_sql('musicians', db_conn, index=False, if_exists='append')
+        db_conn.close()
     
 def populate_musicians_table(musicians_names):
     """
@@ -54,3 +61,8 @@ def populate_musicians_table(musicians_names):
             create_popular_musician_record(musician_name)
     else:
         print('No popular musicians')
+
+        
+if __name__ == "__main__":
+    musicians_names = list(pd.read_csv('artists_names.csv', header=None)[0].values)
+    populate_musicians_table(musicians_names)
