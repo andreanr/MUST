7999ef1865f3f325f0c23e3e011b9c9afa23e6b1
Omar Pardo <omarpardog@gmail.com>
2019-04-09 16:26:29 -0400

    Offer the users the option to add any musician they haven't selected yet.

--- app/routes.py
+++ app/routes.py
@@ -7,8 +7,8 @@ from flask_login import current_user, login_user, logout_user, login_required, U
 from dotenv import load_dotenv, find_dotenv
 from app.sql_aux import (validate_username, add_user, validate_login,
         validate_email, musicians_list, movie_artists_list, get_concerts,
-        get_new_music, get_new_movies, add_music_preference, validate_musician,
-        delete_music_preference, get_user_musicians)
+        get_new_music, get_new_movies, add_music_preference, delete_music_preference,
+        get_user_musicians, get_user_musicians_complement)
 
 from app import app, login_manager
 
@@ -103,17 +103,15 @@ def homepage():
 @app.route('/add_musician', methods=['GET', 'POST'])
 def add_musician():
     if current_user.is_authenticated:
-        if request.method == 'GET':
-            return render_template('add_musician.html')
         username = current_user.id
+        if request.method == 'GET':
+            user_musicians_complement = get_user_musicians_complement(username)
+            return render_template('add_musician.html', user_musicians_complement=user_musicians_complement)
         sp_id = request.form['sp_id']
-        if not validate_musician(username, sp_id):
-            flash('Musician previously registered')
-            return render_template('add_musician.html')
-        else:
-            add_music_preference(username, sp_id)
-            flash('Musician successfully registered')
-            return render_template('add_musician.html')
+        add_music_preference(username, sp_id)
+        user_musicians_complement = get_user_musicians_complement(username)
+        flash('Musician successfully added')
+        return render_template('add_musician.html', user_musicians_complement=user_musicians_complement)
 
 @login_required
 @app.route('/delete_musician', methods=['GET', 'POST'])
